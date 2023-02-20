90cd73563a1b24f30cb2e33e6836bdcd541676b3
valentina pulgarin <30valep@gmail.com>
2023-02-20 13:53:03 -0500

    probandoEstilos

--- platziMaterialUI/src/components/PrincipalInformacion.jsx
+++ platziMaterialUI/src/components/PrincipalInformacion.jsx
@@ -1,21 +1,58 @@
-import { Typography } from "@mui/material"
-import { Stack } from "@mui/system"
+import * as React from 'react';
+import {  BottomNavigation, BottomNavigationAction, Grid, Typography } from "@mui/material"
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import { Diversity3, DynamicFeed } from '@mui/icons-material';
+
 
 
 export const PrincipalInformacion = ({userData}) => {
+    
+  const [value, setValue] = React.useState(0);
 
-    const {name, login, created_at} = userData;
+    const {name, login, created_at,bio, public_repos , followers, following } = userData;
 
 
   return (
     <>
-    <Stack>
-        <Typography> {name} </Typography>
-        <Typography> {created_at} </Typography>
-    </Stack>
+      <Grid 
+      container
+      direction='row'
+      justifyContent='space-between'
+      >
+        <Grid item >
+          <Typography> {name} </Typography>
+          <Typography> {created_at} </Typography>
+        </Grid>
+       
 
-    <Typography> {login} </Typography>
-    
+      <Typography> {login} </Typography>
+      <Typography> {bio} </Typography>
+
+    <Grid item>
+      
+    </Grid>
+
+    <Grid item >
+      <Typography> repos</Typography>
+      <Typography> followers</Typography>
+      <Typography> following</Typography>
+      <BottomNavigation
+        showLabels
+        value={value}
+        onChange={(event, newValue)=>{
+          setValue(newValue);
+        }}
+      >
+
+        <BottomNavigationAction  label={ public_repos} icon={<FavoriteIcon/>} /> 
+        <BottomNavigationAction label={ followers} icon={<FavoriteIcon  />}/>
+        <BottomNavigationAction label={ following} icon={<FavoriteIcon  />}/>
+        
+      </BottomNavigation>
+    </Grid>
+    </Grid>
+   
+
     </>
   )
 }
